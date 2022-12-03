4043f55cc74bd1544faa7c32ab51703dc84d9ccb
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2022-12-03 17:33:22 -0500

    Update Scratch.py

---
+++ Scratch.py
@@ -0,0 +1,37 @@
+# insertion sort: find the minimum in unsorted and swap down until corect
+
+def ins_sort(arr):
+    for x in range(0, len(arr) - 1):
+        y = x + 1
+        while y > 0:
+            if arr[y] < arr[y - 1]:
+                temp = arr[y]
+                arr[y] = arr[y - 1]
+                arr[y - 1] = temp
+                y -= 1
+            else:
+                break
+    return arr
+
+
+# selection sort: find the minimum in unsorted and swap down instantly
+def sel_sort(arr):
+    x = 0
+    while x < len(arr):
+        min_pos = x
+        for y in range(x, len(arr)):
+            if arr[y] < arr[min_pos]:
+                min_pos = y
+        temp = arr[min_pos]
+        arr[min_pos] = arr[x]
+        arr[x] = temp
+        x += 1
+    return arr
+
+
+test_arr = [1, 12, 9, 7, 3, 6, 11]
+
+
+print(ins_sort(test_arr))
+print(sel_sort(test_arr))
+
